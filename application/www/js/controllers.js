c299ac4662f369925761341cfe4a406858cc3fdd
Aaron Faber <aaron.faber@digitaslbi.com>
2015-03-01 20:50:16 +0000

    Added ability to create list

--- application/www/js/controllers.js
+++ application/www/js/controllers.js
@@ -1,5 +1,4 @@
 angular.module('andy.controllers', [])
 
 .controller('AppCtrl', function($scope) {
-  console.log('App controller');
 });
---
+++ application/www/js/directives.js
@@ -0,0 +1,32 @@
+angular.module('andy.directives', [])
+
+.directive('andyShoppingList', shoppingList);
+
+function shoppingList () {
+  var directive = {
+    restrict: 'E',
+    replace: true,
+    templateUrl: 'templates/partials/shopping-list.html',
+    scope: {},
+    link: shoppingListLink
+  };
+
+  return directive;
+}
+
+function shoppingListLink (scope, element) {
+  scope.shoppingList = [];
+
+  scope.addShoppingListItem = function () {
+    scope.shoppingList.push(scope.itemInput);
+    scope.itemInput = '';
+  }
+
+  scope.clearShoppingList = function () {
+    console.log('clear shopping list');
+  }
+
+  scope.editItem = function () {
+    console.log('edit item');
+  }
+}
